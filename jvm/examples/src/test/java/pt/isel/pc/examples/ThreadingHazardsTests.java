f7d3a291f17e8cb18ecc64e7b96315cc4c177cc7
Pedro Felix <pmhsfelix@gmail.com>
2020-10-05 16:28:03 +0100

    adds concurrent data structure manipulation hazards

--- jvm/examples/src/test/java/pt/isel/pc/examples/ThreadingHazardsTests.java
+++ jvm/examples/src/test/java/pt/isel/pc/examples/ThreadingHazardsTests.java
@@ -13,8 +13,8 @@ import static org.junit.Assert.assertNotEquals;
 
 public class ThreadingHazardsTests {
 
-    private static final int N_OF_REPS = 50000;
-    private static final int N_OF_THREADS = 3;
+    private static final int N_OF_REPS = 500000;
+    private static final int N_OF_THREADS = 10;
 
     private int simpleCounter = 0;
 
@@ -215,6 +215,119 @@ public class ThreadingHazardsTests {
                 .reduce(0, Integer::sum);
 
         assertEquals(N_OF_THREADS * N_OF_REPS, totalCount);
+    }
+
+    public static class SimpleLinkedStack<T> {
+
+        static class Node<T> {
+            final T item;
+            final Node<T> next;
+
+            Node(T item, Node<T> next) {
+                this.item = item;
+                this.next = next;
+            }
+        }
+
+        Node<T> head = null;
+
+        void push(T value) {
+            Node<T> node = new Node<T>(value, head);
+            head = node;
+        }
+
+        Optional<T> pop() {
+            Node<T> observedHead = head;
+            if (observedHead == null) {
+                return Optional.empty();
+            }
+            head = observedHead.next;
+            return Optional.of(observedHead.item);
+        }
+
+        boolean isEmpty() {
+            return head == null;
+        }
+    }
+
+    private final SimpleLinkedStack<Integer> nonThreadSafelist = new SimpleLinkedStack<>();
+
+    @Test
+    public void loosing_items_on_a_linked_list() {
+
+        List<Thread> ths = new ArrayList<>(N_OF_THREADS);
+        for (int i = 0; i < N_OF_THREADS; ++i) {
+            Thread th = new Thread(() -> {
+                // producer
+                for (int j = 0; j < N_OF_REPS; ++j) {
+                    nonThreadSafelist.push(1);
+                }
+            });
+            th.start();
+            ths.add(th);
+        }
+
+        ths.forEach(TestUtils::uninterruptibleJoin);
+        int acc = 0;
+        while (!nonThreadSafelist.isEmpty()) {
+            acc += nonThreadSafelist.pop().orElseThrow();
+        }
+
+        // notice that the assertion is NOT equals
+        assertNotEquals(N_OF_THREADS * N_OF_REPS, acc);
+    }
+
+    static class SynchronizedLinkedStack<T> {
+
+        private final SimpleLinkedStack<T> stack = new SimpleLinkedStack<>();
+        private final Object lock = new Object();
+
+        void push(T item) {
+            synchronized (lock) {
+                stack.push(item);
+            }
+        }
+
+        Optional<T> pop() {
+            synchronized (lock) {
+                return stack.pop();
+            }
+        }
+
+        boolean isEmpty() {
+            synchronized (lock) {
+                return stack.isEmpty();
+            }
+        }
+    }
+
+    private final SynchronizedLinkedStack<Integer> synchronizedLinkedStack = new SynchronizedLinkedStack<>();
+
+    @Test
+    public void not_loosing_items_on_a_synchronized_list() {
+
+        AtomicInteger counter = new AtomicInteger(0);
+
+        List<Thread> ths = new ArrayList<>(N_OF_THREADS);
+        for (int i = 0; i < N_OF_THREADS ; ++i) {
+
+            Thread th = new Thread(() -> {
+                // producer
+                for (int j = 0; j < N_OF_REPS; ++j) {
+                    synchronizedLinkedStack.push(1);
+                }
+            });
+            th.start();
+            ths.add(th);
+        }
+
+        ths.forEach(TestUtils::uninterruptibleJoin);
+
+        int acc = 0;
+        while (!synchronizedLinkedStack.isEmpty()) {
+            acc += synchronizedLinkedStack.pop().orElseThrow();
+        }
+        assertEquals(N_OF_THREADS * N_OF_REPS, acc);
     }
 
 }
